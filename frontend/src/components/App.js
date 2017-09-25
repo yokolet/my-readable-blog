a833ba4ba0898393fad791122d8a5c822c822bbb
Yoko Harada <yokolet@gmail.com>
2017-09-26 00:25:41 +0900

    feat: adds navbar and header
    This is a WIP in style commit. The navbar and header have been added
    with responsive feature.

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import BasicNavbar from './BasicNavbar';
 import * as API from '../utils/api'
 
 class App extends Component {
@@ -30,9 +31,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <BasicNavbar />
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <div>Welcome to Readable Blog</div>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
