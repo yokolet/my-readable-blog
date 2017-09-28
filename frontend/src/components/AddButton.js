15ec0e206fe9a7c544395c15176c1c6e402c6a69
Yoko Harada <yokolet@gmail.com>
2017-09-28 22:37:54 +0900

    feat: makes add button to open new post modal window
    The new post modal window also opens by clicking the plus button.

--- frontend/src/components/AddButton.js
+++ frontend/src/components/AddButton.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
 import { Button  } from 'react-bootstrap'
+import { setVisibilityNewPostModal } from '../actions'
 
 class AddButton extends Component {
   render() {
+    const { setModalOpen } = this.props
     return (
       <div className="new-post">
-        <Button className="btn btn-lg btn-info btn-circle">
+        <Button className="btn btn-lg btn-info btn-circle"
+                onClick={e => {
+                  e.preventDefault()
+                  setModalOpen(true)
+                }}>
           <i className="fa fa-plus"></i>
         </Button>
       </div>
@@ -14,4 +21,24 @@ class AddButton extends Component {
   }
 }
 
-export default AddButton
+AddButton.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  setModalOpen: PropTypes.func.isRequired,
+}
+
+function mapStateToProps({visibilityNewPostModal}) {
+  return {
+    isOpen: visibilityNewPostModal.open,
+  }
+}
+
+function mapDispatchToProps (dispatch) {
+  return {
+    setModalOpen: open => dispatch(setVisibilityNewPostModal(open))
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AddButton);
