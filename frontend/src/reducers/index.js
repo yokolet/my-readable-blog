65f934b0303e31a59665769498be613308e3b71f
Yoko Harada <yokolet@gmail.com>
2017-09-29 21:21:38 +0900

    chore: on the way to merge posts to all_posts
    It turned out, all posts related reducer functions should be in a single
    file. Since all those update posts data, reducers should use a single
    state which has post data.

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux'
 import allCategories from './all_categories'
 import allPosts from './all_posts'
-import posts from './posts'
 import comments from './comments'
 import visibilityCategory from './visibility_category'
 import visibilityNewPostModal from './visibility_newpost_modal'
@@ -11,6 +10,5 @@ export default combineReducers({
   allPosts,
   visibilityCategory,
   visibilityNewPostModal,
-  posts,
   comments,
 })
