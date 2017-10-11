fd12224b435c9bf7712f2e638435cc912da27027
Yoko Harada <yokolet@gmail.com>
2017-10-11 22:31:41 +0900

    chore: changes var to let
    According to the suggestion, var is an old school back in 2013. Thinking
    of the scope, those var should be let these days.

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -14,7 +14,7 @@ class Header extends Component {
 
   render() {
     const { categories, setModalOpen, changeSortBy, location } = this.props
-    var headerContent
+    let headerContent
     if (location === 'home' || location === 'category') {
       headerContent =
         <div className="App-header">
--- frontend/src/utils/helpers.js
+++ frontend/src/utils/helpers.js
@@ -1,7 +1,7 @@
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
                 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 export function millisToDate (millis) {
-  var date = new Date(millis)
+  let date = new Date(millis)
   return months[date.getUTCMonth()] + ' ' +
               date.getUTCDate() + ', ' +
               date.getUTCFullYear();
